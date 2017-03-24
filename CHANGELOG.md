42f29b028b6f369f8107092a50e30b0dc6786182
kbukum <kamilbukum@gmail.com>
2017-03-24 22:29:13 +0300

    #### v1.0.0  Developing ... * create skeleton typescript library as initial project. * added docs and comments to **app/App** class.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,5 @@
 ## CHANGE LOG
 
-#### v1.0.5  Developing ...
+#### v1.0.0  Developing ...
 * create skeleton typescript library as initial project.
+* added docs and comments to **app/App** class.
--- src/app/App.ts
+++ src/app/App.ts
@@ -1,7 +1,20 @@
+/**
+ * This is a comment which explains the aim of the class
+ * @export
+ * @default App
+ */
 export default class App {
+    /**
+     * This is a comment which explains the aim or usage of the @see getText() method
+     * @return {string}
+     */
     public getText(): string{
         return "Hello World";
     }
+
+    /**
+     * This is a comment which explains the aim or usage of the @see show() method
+     */
     public show(){
         console.log(this.getText());
     }
